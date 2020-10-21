2a1a0ba288ca3d613ce8a6b898875a9a5c535113
Jay Taves <jht99@cornell.edu>
2020-10-21 17:40:28 -0500

    Still debugging kinematics
    -- Created separate file for inverse dynamics analysis

--- hw7/simEngine3D_A7P1.py
+++ hw7/simEngine3D_A7P1.py
@@ -1,5 +1,102 @@
-from simEngine3D_A6P2 import *
+from gcons import *
+import sympy as sp
 import matplotlib.pyplot as plt
+
+# Parameters
+L = 2
+t0 = 0
+Δt = 10e-3
+t_end = 10
+
+# Set up driving constraint
+t = sp.symbols('t')
+θ_sym = sp.pi/2 + sp.pi/4 * sp.cos(2*t)
+f_sym = sp.cos(θ_sym)
+df_sym = sp.diff(f_sym, t)
+ddf_sym = sp.diff(df_sym, t)
+
+θ = sp.lambdify(t, θ_sym)
+f = sp.lambdify(t, f_sym)
+df = sp.lambdify(t, df_sym)
+ddf = sp.lambdify(t, ddf_sym)
+
+# Read from file, set up bodies + constraints
+(bodies, constraints) = ReadModelFile('hw6/revJoint.mdl')
+
+pendulum = Body(bodies[0])
+ground = Body({}, True)
+
+# Driving DP1 constraint
+dp1_drive = CreateConstraint(constraints[0], pendulum, ground)
+
+# Manually add these functions rather than have the parser do it
+dp1_drive.f = f
+dp1_drive.df = df
+dp1_drive.ddf = ddf
+
+# DP1 Constraints
+dp1_xx = CreateConstraint(constraints[1], pendulum, ground)
+dp1_yx = CreateConstraint(constraints[2], pendulum, ground)
+
+# CD ijk Constraints
+cd_i = CreateConstraint(constraints[3], pendulum, ground)
+cd_j = CreateConstraint(constraints[4], pendulum, ground)
+cd_k = CreateConstraint(constraints[5], pendulum, ground)
+
+cd_i.si = np.array([[-L], [0], [0]])
+cd_j.si = np.array([[-L], [0], [0]])
+cd_k.si = np.array([[-L], [0], [0]])
+
+# Signs flipped here from what you might intuitively expect
+# because of the direction we take the difference (0 - a) vs. (a - 0)
+# CD_con_j = -L * sp.sin(θ_sym)
+# CD_con_k = L * sp.cos(θ_sym)
+
+# cd_j.f = sp.lambdify(t, CD_con_j)
+# cd_j.df = sp.lambdify(t, sp.diff(CD_con_j, t))
+# cd_j.ddf = sp.lambdify(t, sp.diff(CD_con_j, t, t))
+
+# cd_k.f = sp.lambdify(t, CD_con_k)
+# cd_k.df = sp.lambdify(t, sp.diff(CD_con_k, t))
+# cd_k.ddf = sp.lambdify(t, sp.diff(CD_con_k, t, t))
+
+# Euler Parameter Constraint
+e_param = EulerCon(pendulum)
+
+# Derived initializations
+θ0 = θ(t0)
+r0 = np.array([[0], [L * np.sin(θ0)], [L * np.cos(θ0)]])
+z_axis = np.array([[0], [0], [1]])
+y_axis = np.array([[0], [1], [0]])
+p0 = (RotAxis(y_axis, np.pi/2) * RotAxis(z_axis, θ0 - np.pi/2)).arr
+
+# Didn't read these in from the file, so set them now
+pendulum.r = r0
+pendulum.p = p0
+pendulum.dp = 1/2 * E(p0).T @ (dp1_drive.df(t0) * z_axis)
+
+# Group our constraints together
+g_cons = ConGroup([cd_i, cd_j, cd_k, dp1_xx, dp1_yx, dp1_drive, e_param])
+
+# # Compute values
+Φ = g_cons.GetPhi(t0)
+nu = g_cons.GetNu(t0)  # Darn, ν ≈ v in my font
+γ = g_cons.GetGamma(t0)
+Φ_r = g_cons.GetPhiR(t0)
+Φ_p = g_cons.GetPhiP(t0)
+Φ_q = g_cons.GetPhiQ(t0)
+
+print(Φ)
+# print(nu)
+print(γ)
+# print(Φ_r)
+# print(Φ_p)
+# print(Φ_q)
+
+# print(r0)
+# print(p0)
+
+q0 = np.concatenate((r0, p0), axis=0)
 
 
 def NextNewton(phi, inv_phi_q, q_k):
@@ -12,6 +109,8 @@ tol = 1e-5
 t_steps = int(t_end/Δt)
 t_grid = np.linspace(t_start, t_end, t_steps, endpoint=True)
 max_iters = 500
+
+M = np
 
 # Get arrays to hold O and Q data
 O_pos = np.zeros((t_steps, 3))
